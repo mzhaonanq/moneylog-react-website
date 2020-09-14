d8386203b2745ccf6ebd8dc52d02b37ab921e5ed
mzhaonanq <1002604003@qq.com>
2020-09-14 21:30:41 +0800

    deploy

---
+++ precache-manifest.c56f88aa891acd1a783af1d74b96f754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1af822dd8a2ddcd3f9e819b42b5385",
+    "url": "/moneylog-react-website/index.html"
+  },
+  {
+    "revision": "1d223a7eba0cc53d48d9",
+    "url": "/moneylog-react-website/static/css/main.9ad33c9f.chunk.css"
+  },
+  {
+    "revision": "1025ac761343d02619bf",
+    "url": "/moneylog-react-website/static/js/2.f859e049.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/moneylog-react-website/static/js/2.f859e049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d223a7eba0cc53d48d9",
+    "url": "/moneylog-react-website/static/js/main.7bfeb80d.chunk.js"
+  },
+  {
+    "revision": "b3b5b25f4e713030d64e",
+    "url": "/moneylog-react-website/static/js/runtime-main.ddff77cd.js"
+  }
+]);
